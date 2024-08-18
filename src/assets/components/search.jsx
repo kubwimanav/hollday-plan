2ce46a5bdd96217efe119cfe01f9888a066e6c2d
kubwimanav <kubwimanavincent2020@gmail.com>
2024-08-18 20:16:24 +0200

    fixerr

--- src/assets/components/search.jsx
+++ src/assets/components/search.jsx
@@ -5,30 +5,26 @@ import {MdCancel} from "react-icons/md"
 import './Search.css'
 import white from '../images/white.png'
 import { Link } from "react-router-dom"
-function Search ({setModal}) {
-    return(
-        <div className="search">
-     <div className="top">
-        
-        <img src={white} alt="Top Image" /> 
-         <MdCancel className="BOTH" onClick={()=> setModal(false)}/> 
-        </div>  
-    
+function Search({ setModal, handlemadal }) {
+  return (
+    <div className="search">
+      <div className="top">
+        <img src={white} alt="Top Image" />
+        <MdCancel className="BOTH" onClick={handlemadal} />
+      </div>
 
-    
-    <div class="navigation">
-        <Link to="homepage">Home</Link>
-        <Link to="tour">Tour</Link>
-        <Link to="login">Login</Link>
-        <Link to="contact_page">Contact Us</Link>
+      <div class="navigation">
+        <Link to="homepage" onClick={handlemadal}>Home</Link>
+        <Link to ="tour" onClick={handlemadal}>Tour</Link>
+        <Link to="login" onClick={handlemadal}>Login</Link>
+        <Link to="contact_page" onClick={handlemadal}>Contact Us</Link>
+      </div>
+
+      <div class="social-media">
+        <BsFacebook />
+        <AiFillInstagram />
+        <AiFillTwitterCircle />
+      </div>
     </div>
-
-
-    <div class="social-media">
-          <BsFacebook/>
-           <AiFillInstagram />
-           <AiFillTwitterCircle  />
-    </div>
-        </div>
-    )
+  );
 } export default Search
